f0607e7ef957b9cbf19690e70f9600720557cf20
pdostal <git@pdostal.cz>
2013-12-24 14:51:38 +0100

    Add navbar and colors

--- config_example.php
+++ config_example.php
@@ -1,5 +1,6 @@
 <?php
 	$config['url'] = array('');
+	$config['group'][] = '';
 	$config['service'][] = array('group' => '', 'id' => '', 'name' => '');
 	$config['time'] = array('day', 'week', 'month', 'year');
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
   require_once('config.php');
+  function randomColors($range) { 
+    $range = $range[rand(0,count($range)-1)];
+    $color = str_split($range, 2);
+    $background = '#';
+    $foreground = '#';
+    for($i=0;$i<3;$i++) {
+      if (empty($range)) {
+        $rand = rand(0,255);
+      } else {
+        $rand = hexdec($color[$i]);
+      }
+      if ($rand < 16) $background .= '0';
+      $background .= dechex($rand);
+      if ($rand > 239) $foreground .= '0';
+      $foreground .= dechex(255-$rand);
+    }
+    return array($background, $foreground);
+  }
+  $data = exec('uptime');
+  $uptime = preg_replace('/^.+ up |, .+$/i', ' ', $data);
+  $load = explode(' ', $data);
+  $load = $load[count($load)-3]." ".$load[count($load)-2]." ".$load[count($load)-1];
+  $uname = php_uname();
+  $uname = explode(' ', $uname);
+  $uname = $uname[2];
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,17 +39,46 @@
     <![endif]-->
   </head>
   <body>
-    <section class="container">
-      <article>
-        <h1><?php echo gethostname(); ?></h1>
+    <nav class='navbar navbar-default navbar-fixed-top' role='navigation'>
+      <div class='navbar-header'>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <h1 class='navbar-brand'><?php echo gethostname(); ?></h1>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+<?php
+  $i = 0;
+  foreach ($config['group'] as $group) {
+    echo "<li><a href='#section".$i."'>".$group."</a></li>\n";
+    $i++;
+  }
+?>
+        </ul>
+        <div class="nav navbar-nav navbar-right">
+          <div class="navbar-text"><span>Uptime: </span><span><?php echo $uptime; ?></span></div>
+          <div class="navbar-text"><span>Uname: </span><span><?php echo $uname; ?></span></div>
+          <div class="navbar-text"><span>Load: </span><span><?php echo $load; ?></span></div>
+        </div>
+    </div>
+  </nav>
 <?php
   foreach ($config['url'] as $url) {
     $i = 0;
     $j = 0;
     foreach ($config['service'] as $service) {
+      if ( $config['service'][$j]['group'] !== $config['service'][$j-1]['group'] ) {
+        $rand = randomColors(array('AAAAAA', 'BBBBBB', 'CCCCCC', 'DDDDDD', 'EEEEEE'));
+        echo "\t\t<section id='section".$service['group']."' style='background-color: ".$rand[0]."; color: ".$rand[1].";'>\n";
+        echo "\t\t<div class='container'>\n";
+      }
       if ($i==0) echo "\t\t<div class='row'>\n";
       echo "\t\t\t<div class='col-md-4 munin_plugin'>\n";
-      echo "\t\t\t\t<h2>".$service['name']."</h2>\n";
+      echo "\t\t\t\t<h2 style='color: ".$rand[1].";'>".$service['name']."</h2>\n";
       foreach ($config['time'] as $time) {
         echo "\t\t\t\t<img src='".$url."/".$service['id']."-".$time.".png' alt='' />\n";
       }
@@ -35,13 +89,18 @@
       } else {
         $i++;
       }
-      if ( $config['service'][$j]['group'] !== $config['service'][$j+1]['group'] ) echo "\t\t<hr />\n";
+      if ( $config['service'][$j]['group'] !== $config['service'][$j+1]['group'] ) {
+        echo "\t\t<hr />\n";
+        echo "\t\t</div>\n";
+        echo "\t\t</section>\n";
+      }
       $j++;
     }
   }
 ?>
-      </article>
-    </section>
+    <footer class='container'>
+      <span>Source: <a href='<?php echo $config['url']; ?>'>Munin</a>
+    </footer>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
