3805d8e1747679f66b52ff01982ff619c6fbee8e
pdostal <git@pdostal.cz>
2013-12-26 11:54:37 +0100

    Simplify color and fix height and header font size

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
   require_once('config.php');
-  function randomColors($range) { 
-    $range = $range[rand(0,count($range)-1)];
-    $color = str_split($range, 2);
-    $background = '#';
-    $foreground = '#';
-    for($i=0;$i<3;$i++) {
-      if (empty($range)) {
-        $rand = rand(0,255);
-      } else {
-        $rand = hexdec($color[$i]);
-      }
-      if ($rand < 16) $background .= '0';
-      $background .= dechex($rand);
-      if ($rand > 239) $foreground .= '0';
-      $foreground .= dechex(255-$rand);
-    }
-    return array($background, $foreground);
-  }
   $data = exec('uptime');
   $uptime = preg_replace('/^.+ up |, .+$/i', ' ', $data);
   $load = explode(' ', $data);
@@ -54,7 +36,7 @@
 <?php
   $i = 0;
   foreach ($config['group'] as $group) {
-    echo "\t\t\t<li><a href='#section".$i."'>".$group."</a></li>\n";
+    echo "\t\t\t<li><a href='#sensor".$i."'>".$group."</a></li>\n";
     $i++;
   }
 ?>
@@ -72,13 +54,12 @@
     $j = 0;
     foreach ($config['service'] as $service) {
       if ( $config['service'][$j]['group'] !== $config['service'][$j-1]['group'] ) {
-        $rand = randomColors(array('AAAAAA', 'BBBBBB', 'CCCCCC', 'DDDDDD', 'EEEEEE'));
-        echo "\t\t<section id='section".$service['group']."' style='background-color: ".$rand[0]."; color: ".$rand[1].";'>\n";
+        echo "\t\t<section class='sensor' id='sensor".$service['group']."'>\n";
         echo "\t\t\t<div class='container'>\n";
       }
       if ($i==0) echo "\t\t\t\t<div class='row'>\n";
       echo "\t\t\t\t\t<div class='col-md-4 munin_plugin'>\n";
-      echo "\t\t\t\t\t\t<h2 style='color: ".$rand[1].";'>".$service['name']."</h2>\n";
+      echo "\t\t\t\t\t\t<h2>".$service['name']."</h2>\n";
       foreach ($config['time'] as $time) {
         echo "\t\t\t\t\t\t<div class='graph_wrapper'><img src='".$url."/".$service['id']."-".$time.".png' alt='' /></div>\n";
       }
